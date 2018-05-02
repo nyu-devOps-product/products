bb2878b8578defeae58b60fb833e88f86925f0ff
Lei Guo <lg2681@nyu.edu>
2018-05-02 13:21:34 -0400

    fixed error in test_server

--- tests/test_server.py
+++ tests/test_server.py
@@ -222,7 +222,7 @@ class TestProductServer(unittest.TestCase):
         """ Get one product with keyword """
         resp = self.app.get('/products', query_string='name=iPhone')
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        logging.info('resp.data length: ' + str(len(resp.data)))
+        logging.info('resp.data length: ' + str(len(json.loads(resp.data))))
         self.assertTrue(len(resp.data) > 0)
         logging.info('resp.data: ' + str(json.loads(resp.data)))
         self.assertIn('iPhone 8', resp.data)
